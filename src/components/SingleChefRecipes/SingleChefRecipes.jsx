406b4bd71769b917e20ab9293466d47f94b3c50e
smhasanjamil <smhasanjamil23@gmail.com>
2023-05-04 17:12:50 +0600

    Disable favourite button after click

--- src/components/SingleChefRecipes/SingleChefRecipes.jsx
+++ src/components/SingleChefRecipes/SingleChefRecipes.jsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const SingleChefRecipes = ({ rec }) => {
-    console.log(rec)
+    const [disabled, setDisabled] = useState(false);
+    // console.log(rec)
+
+    const handleOnClick = () => {
+        setDisabled(true);
+    }
     return (
         <div>
 
@@ -11,10 +16,13 @@ const SingleChefRecipes = ({ rec }) => {
                 <div className="card-body">
                     <h2 className="card-title">{rec.name}</h2>
                     <p className='font-bold'>Ingredients:</p>
-                    <p>{rec?.ingredients?.map(data=><li>{data}</li>)}</p>
+                    <p>{rec?.ingredients?.map(data => <li>{data}</li>)}</p>
                     <p className='font-bold'>Cooking method:</p>
                     <p>{rec.method}</p>
                     <p className='font-bold'>Rating : {rec.rating}</p>
+                    <div className="card-actions">
+                        <button disabled={disabled} onClick={handleOnClick} className="btn">Add to Favourite</button>
+                    </div>
                 </div>
             </div>
 
