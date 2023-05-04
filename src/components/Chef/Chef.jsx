aa9ef66b721d05cb5fd4898fdc309687e0e05c20
smhasanjamil <smhasanjamil23@gmail.com>
2023-05-04 20:55:22 +0600

    Our Services Added

--- src/components/Chef/Chef.jsx
+++ src/components/Chef/Chef.jsx
@@ -18,44 +18,51 @@ const Chef = () => {
     // console.log(chefData);
 
     return (
-        <div className='mx-auto container my-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+        <div className='mx-auto container my-10 '>
+            <div className='text-center text-5xl font-bold underline'><h1>Chef</h1></div>
+            <div>
+                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
 
-            {chefData?.map((allchef) => (
-                <div key={allchef.id}>
 
-                    <div className="card w-96 bg-base-100 shadow-xl">
-                        <figure className="px-10 pt-10">
-                            <img src={allchef.picture} alt="Shoes" className="rounded-xl" />
-                        </figure>
-                        <div className="card-body items-center text-center">
-                            <h2 className="card-title">{allchef.name}</h2>
-                            <p>Experience: {allchef.experience} Year</p>
-                            <p>Number Of Recipes: {allchef.numRecipes} </p>
-                            <div>
-                                <Rating
-                                    readonly
-                                    placeholderRating={allchef.likes}
-                                    emptySymbol={<FaRegStar />}
+                    {chefData?.map((allchef) => (
+                        <div key={allchef.id}>
 
-                                    placeholderSymbol={<FaStar />}
-                                    fullSymbol={<FaStar />}
-                                ></Rating>
-                                <span>{allchef.likes}</span>
+                            <div className="card w-96 bg-base-100 shadow-xl">
+                                <figure className="px-10 pt-10">
+                                    <img src={allchef.picture} alt="Shoes" className="rounded-xl" />
+                                </figure>
+                                <div className="card-body items-center text-center">
+                                    <h2 className="card-title">{allchef.name}</h2>
+                                    <p>Experience: {allchef.experience} Year</p>
+                                    <p>Number Of Recipes: {allchef.numRecipes} </p>
+                                    <div>
+                                        <Rating
+                                            readonly
+                                            placeholderRating={allchef.likes}
+                                            emptySymbol={<FaRegStar />}
+
+                                            placeholderSymbol={<FaStar />}
+                                            fullSymbol={<FaStar />}
+                                        ></Rating>
+                                        <span>{allchef.likes}</span>
+                                    </div>
+
+                                    <div className="card-actions">
+                                        <Link to={`/chef/${allchef?.id}`} className="btn">View Recipes</Link>
+                                    </div>
+                                </div>
                             </div>
 
-                            <div className="card-actions">
-                                <Link to={`/chef/${allchef?.id}`} className="btn">View Recipes</Link>
-                            </div>
                         </div>
-                    </div>
+                    ))}
+
+
+
 
                 </div>
-            ))}
-
-
-
-
+            </div>
         </div>
+
     );
 };
 
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -23,6 +23,54 @@ const Home = () => {
 
             <Chef />
 
+
+
+            <div className='mx-auto container mt-10'>
+                <div className='font-bold text-lg text-center'>
+                    <h1>Our Services</h1>
+                </div>
+                <div className='mb-5'>
+                    <h1 className='font-bold text-3xl text-center mt-10'>Your Go-To Food Delivery Partner</h1>
+                    <p className='text-base text-center'>Our delivery service is lightning-fast, ensuring that you never have to wait long for your food to arrive. From the moment you place your order to the second it's delivered to your doorstep, we strive to make the process as quick and efficient as possible.</p>
+                </div>
+
+
+                <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                    <div class="p-4">
+                        <div className="card w-full">
+                            <figure><img src="https://i.ibb.co/YXkZ2XH/01.jpg" alt="Shoes" /></figure>
+                            <div className="card-body">
+                                <h2 className="card-title">Fastest Delivery</h2>
+                                <p>Our delivery service is lightning-fast, ensuring that you never have to wait long for your food to arrive. From the moment you place your order to the second it's delivered to your doorstep, we strive to make the process as quick and efficient as possible.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="p-4">
+                        <div className="card w-full">
+                            <figure><img src="https://i.ibb.co/LCr25BW/02.jpg" alt="Shoes" /></figure>
+                            <div className="card-body">
+                                <h2 className="card-title">Wide Variety of Cuisine</h2>
+                                <p>With a vast selection of cuisines from all over the world, you're sure to find something you'll love on our menu. From classic comfort food to exotic dishes you've never tried before, we've got you covered.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="p-4">
+                        <div className="card w-full ">
+                            <figure><img src="https://i.ibb.co/b2RMDhb/03.jpg" alt="Shoes" /></figure>
+                            <div className="card-body">
+                                <h2 className="card-title">Easy Ordering Process</h2>
+                                <p>Our ordering process is simple and hassle-free. Whether you prefer to order online, by phone, or through our mobile app, we make it easy to get the food you crave without any unnecessary complications.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+
+            </div>
+
+
+
         </div>
     );
 };
