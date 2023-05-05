99d522ffa72934d1f70ead68ff8c0a693723fc90
smhasanjamil <smhasanjamil23@gmail.com>
2023-05-05 15:38:12 +0600

    Not found page update with image

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './NotFound.css';
 
 const NotFound = () => {
     return (
-        <div>
-            
-            <div class="flex flex-col gap-4 justify-center items-center h-screen">
-                <h1 className='font-bold text-5xl'>Oops! Page not found.</h1>
-                <p className='text-lg'>It looks like the page you were looking for does not exist.</p>
-                <button className="btn btn-lg"><Link to="/">Back To Home</Link></button>
+        <div className='notfound-container flex justify-center mx-auto container'>
+        
+
+
+            <div className="card w-full">
+                <figure className="px-10 pt-10">
+                    <img src="https://i.ibb.co/88jxg6W/error.jpg" alt="Shoes" className="text h-80 w-auto" />
+                </figure>
+                <div className="card-body items-center text-center">
+                    <h2 className="card-title text-5xl">Oops! Page not found.</h2>
+                    <p>It looks like the page you were looking for does not exist.</p>
+                    <div className="card-actions">
+                    <button className="btn btn-lg bg-indigo-600 border-indigo-600"><Link to="/">Back To Home</Link></button>
+                    </div>
+                </div>
             </div>
+
+
         </div>
     );
 };
